651dcab665efad97af0ab8e06a41055f77b6f97e
Shoaib Shaikh <shoshaikh7@gmail.com>
2021-12-16 18:55:59 -0800

    set up variables for TMDB api

---
+++ 17_movie_app/script.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie/?sort_by=popularity.desc&api_key=79b3e67cc95e570a5f65153fc60370d2&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=79b3e67cc95e570a5f65153fc60370d2&query="';
